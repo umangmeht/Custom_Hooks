669dd303d9e02310a56bc669fe13d53db00faca4
umangmeht <127183723+umangmeht@users.noreply.github.com>
2023-03-07 14:56:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Album from "./Albums";
+import "./styles.css";
+import Todo from "./Todo";
+
+const App = () => {
+  return (
+    <>
+      <Todo />
+      <Album />
+    </>
+  );
+};
+
+export default App;
